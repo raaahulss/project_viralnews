379d8057a9b7baa9bdd8ee0f29196ff17d1c679e
Rahul Salla <raaahulss@gmail.com>
2020-06-02 05:06:58 -0400

    backup existing skeleton

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './component'
+// import App from './baseline_component'
+import App from './components/app'
 ReactDOM.render(
     <App />,
     document.getElementById('root')
