4f3acd0d7b6aa599d9ec796fb510dfd1add8754e
Sam Teplov <samteplov@Albacore.local>
2020-06-23 14:29:48 -0400

    changed navbar color, fixed ML doc text and formatting.

--- ui/src/components/carousel.js
+++ ui/src/components/carousel.js
@@ -20,31 +20,35 @@ class CarouselMl extends Component {
             alt="First slide"
           />
           <Carousel.Caption>
-            <h1>Viral: The top 50% of retweets based off of our dataset</h1>
-              <p>  In the Viral News project, we look at viral news articles, focusing on articles about
-                U.S. politics specifically. We observe the features that make an article prone to
-                going viral. This product is focusing on two target groups: journalists and news
-                consumers. However, we still aim to provide valuable research which helps us better
-                understand what makes certain articles go viral in order to contribute to the fight
-                against disinformation campaigns.</p>
+          <h1 id="h2-title">Article Viralness</h1>
+
+
+              <p>  Using the median number of retweets in our dataset as the threshold
+              between popular and unpopular articles allows us to set up a binary
+              classification problem. We use a combination of metadata features
+              (date published, time published, length of content, etc) and textual
+              features (content, title) to train a neural network that can predict
+              with 60% accuracy if a certain article will go viral. The purpose of
+              this model is to provide insight into what features of an article cause
+              it to go viral. The dataset used to train our model, as well as the retweet count
+              thresholds for various sources can be found in our GitHub Repo.   </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
             src={bias}
-            alt="Third slide"
+            alt="Second slide"
           />
 
           <Carousel.Caption>
-            <h1>Politcal Bias: The magnitude and political direction implicit in an article </h1>
-            <p>This service detects the underlying political sentiment in a U.S. political news
-                      article. We have used various machine learning models to detect whether an
-                      article is leaning more towards democratic or republican. Since our targeted audience
-                      are journalists and general users, we think detecting the political bias with respect to
-                      two major U.S. political parties can provide useful insights for our users. What’s more,
-                      the underlying political bias of a U.S. news article may have a correlation with its
-                      popularity and we plan to identify this potential correlation.</p>
+            <h1 id="h2-title-bias">Political Bias</h1>
+
+            <p>This model detects the underlying political sentiment in a news
+                      article. This is done by training the model on data from the Ideological Book Corpus (IBC) dataset, which
+                      can be found in our Github Repo. Each sentence in the training dataset was hand-labeled using mechanical turk.
+                      This model can be used to analyze either a published or unpublished article. This differs from the other two models since
+                      this model only considers textual features as input (title and content).</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -55,12 +59,13 @@ class CarouselMl extends Component {
           />
 
           <Carousel.Caption>
-          <h1>Public Sentiment: Positive and negative as defined by our dataset </h1>
-          <p> As a part of Viral News project, this service analyzes how the general public
-                      (twitter users) reacts to news articles on U.S. politics. We provide concrete statistics
-                      including percentage of positive opinions and percentage of negative opinions. We
-                      have used various machine learning models to predict a user’s opinion on the
-                      article. These models are specifically trained on the language used in Twitte.</p>
+          <h1 id="h2-title-reac">Public Reaction</h1>
+          <p> This model analyzes how the public is reacting to a published news article.
+              First, we query Twitter using Twitter's premium API to get a sample of tweets that people
+              have posted as responses to the news article. These responses are then fed into our neural
+              network and then the model outputs whether the general public agrees or disagrees with the article.
+              The dataset that was used to train our model can be found in our Github Repo.
+        </p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
