d44c9a98c480c308ae4d1aec5b9714d37a4f781c
FangzhouXie <xiefangzhou1996@gmail.com>
2020-06-30 16:21:55 -0400

    added error for unsupported files

--- webapp/src/error.py
+++ webapp/src/error.py
@@ -9,6 +9,7 @@ error_list = {
     "UNBL_FTCH_NEWS" : ["UNBL_FTCH_NEWS", "Unable to fetch news article."],
     "NO_EMBD_URL" : ["NO_EMBD_URL","The tweet does not have an embeded URL."],
     "FILE_NT_FND" : ["FILE_NT_FND", "File not found in the request."],
+    "FILE_NT_SUP" : ["FILE_NT_SUP", "File type not supported."],
     "URL_NT_FND" : ["URL_NT_FND" , "URL not found"]
     }
 
