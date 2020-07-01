d19661bbd0151b91ca9b693cc86670aeb4c0b3bf
Sam Teplov <samteplov@Albacore.local>
2020-06-30 20:17:03 -0400

    deleted files we dont need

--- ui/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// import App from './baseline_component'
-import App from './components/app'
-import Dashboard from './dashboard'
-
-ReactDOM.render(<App />, document.getElementById('root'));
--- webapp/src/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from src.requestRouter import router
-
-def main(config="default"):
-    app = Flask("IDK")
-    app.register_blueprint(router)
-    print(__name__, config)
-    return app
