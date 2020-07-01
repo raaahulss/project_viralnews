99684cf7af7f488b7e8bf7d73dc347094e8f3014
Rahul Salla <raaahulss@gmail.com>
2020-07-01 11:42:31 -0400

    Fixed NavBar centering issue

--- ui/src/components/navbar.js
+++ ui/src/components/navbar.js
@@ -5,27 +5,29 @@ import '../css/navbar.css'
 class NavBar extends Component {
   render() {
     return (
-      <Nav fluid ariant="pills" className="justify-content-center" id="sticky-nav">
-        <Nav.Item>
-          <Nav.Link href="/">Home</Nav.Link>
-        </Nav.Item>
-        <div className="vline" />
-        <Nav.Item>
-          <Nav.Link href="https://github.com/raaahulss/project_viralnews">Source</Nav.Link>
-        </Nav.Item>
-        <div className="vline" />
-        <Nav.Item>
-          <Nav.Link href="/api">API Documentation</Nav.Link>
-        </Nav.Item>
-        <div className="vline" />
-        <Nav.Item>
-          <Nav.Link href="/ml">ML Documentation</Nav.Link>
-        </Nav.Item>
-        <div className="vline" />
-        <Nav.Item>
-          <Nav.Link href="/about">About</Nav.Link>
-        </Nav.Item>
-      </Nav>
+      <center id="navbar-center">
+        <Nav fluid ariant="pills" className="justify-content-center" id="sticky-nav">
+          <Nav.Item>
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav.Item>
+          <div className="vline" />
+          <Nav.Item>
+            <Nav.Link href="https://github.com/raaahulss/project_viralnews">Source</Nav.Link>
+          </Nav.Item>
+          <div className="vline" />
+          <Nav.Item>
+            <Nav.Link href="/api">API Documentation</Nav.Link>
+          </Nav.Item>
+          <div className="vline" />
+          <Nav.Item>
+            <Nav.Link href="/ml">ML Documentation</Nav.Link>
+          </Nav.Item>
+          <div className="vline" />
+          <Nav.Item>
+            <Nav.Link href="/about">About</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </center>
     );
   }
 }
