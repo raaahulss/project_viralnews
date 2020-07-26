5e9286e80bd21ac470f0944a2553183046124753
FangzhouXie <xiefangzhou1996@gmail.com>
2020-07-26 15:12:11 -0400

    added test for sending requests concurrently

--- webapp/tests/test_latency.py
+++ webapp/tests/test_latency.py
@@ -5,25 +5,57 @@ import numpy as np
 import json
 
 
-class myThread (threading.Thread):
-    def __init__(self, thread_id, name):
+class MyThread (threading.Thread):
+    def __init__(self, thread_id, name, url):
         threading.Thread.__init__(self)
         self.thread_id = thread_id
         self.name = name
+        self.url = url
 
     def run(self):
         print("Starting " + self.name)
-        print_time(self.name)
+        record_time(self.url)
         print("Exiting " + self.name)
 
 
-def print_time(url, latencies, titles):
+def parallel_test():
+    url_num = len(urls)
+    threads = []
+    for i in range(url_num):
+        threads.append(MyThread(i, f"Thread-{i}", urls[i]))
+
+    # Start new Threads
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    for i in range(len(latencies)):
+        print(f"{latencies[i]} s: {responses[i]}")
+
+    print(
+        f"Maximum {np.max(latencies)} s\nMinimum {np.min(latencies)} s\nMean {np.mean(latencies)} s\nMedian {np.median(latencies)} s\nStandard deviation {np.std(latencies)} s"
+    )
+
+
+def record_time(url):
     start_time = datetime.datetime.now()
     response = requests.post(f'http://localhost:5000/api/url?url={url}')
     end_time = datetime.datetime.now()
     delta_time = end_time - start_time
     latencies.append(delta_time.total_seconds())
-    titles.append(response.content)
+    responses.append(response.content)
+
+
+def sequential_test():
+    for url in urls:
+        record_time(url)
+
+    for i in range(len(latencies)):
+        print(f"{latencies[i]} s: {responses[i]}")
+
+    print(
+        f"Maximum {np.max(latencies)} s\nMinimum {np.min(latencies)} s\nMean {np.mean(latencies)} s\nMedian {np.median(latencies)} s\nStandard deviation {np.std(latencies)} s"
+    )
 
 
 if __name__ == '__main__':
@@ -39,17 +71,19 @@ if __name__ == '__main__':
             'https://twitter.com/WSJ/status/1286620915141312515',
             'https://twitter.com/WSJ/status/1286518814189006848'
             ]
-
+    # urls = [
+    #     'https://www.nytimes.com/2020/07/25/us/protests-seattle-portland.html',
+    #     'https://www.nytimes.com/2020/07/26/nyregion/nyc-coronavirus-time-life-building.html?action=click&module=Top%20Stories&pgtype=Homepage',
+    #     'https://www.nytimes.com/2020/07/26/style/functional-fashion.html?action=click&module=Top%20Stories&pgtype=Homepage',
+    #     'https://www.cnn.com/2020/07/26/health/us-coronavirus-sunday/index.html',
+    #     'https://www.cnn.com/2020/07/26/politics/kudlow-republicans-checks-unemployment-cnntv/index.html',
+    #     'https://www.cnn.com/2020/07/26/us/utah-plane-crash-backyard-trnd/index.html',
+    #     'https://www.cnn.com/2020/07/26/health/crip-camp-americans-with-disabilities-act-wellness/index.html',
+    #     'https://www.nbcnews.com/news/nbcblk/john-lewis-civil-rights-giant-cross-selma-bridge-one-final-n1234941',
+    #     'https://www.nbcnews.com/politics/2020-election/pro-trump-super-pac-losing-money-race-democrats-n1234524',
+    #     'https://www.nbcnews.com/politics/congress/white-house-pushes-slashing-expanded-jobless-benefits-despite-rises-unemployment-n1234944'
+    # ]
     latencies = []
-    titles = []
-
-    for url in urls:
-        print_time(url, latencies, titles)
-
-    for i in range(len(latencies)):
-        print(f"{latencies[i]} s: {titles[i]}")
-    print("Exiting Main Thread")
-
-    print(
-        f"Maximum {np.max(latencies)} s\nMinimum {np.min(latencies)} s\nMean {np.mean(latencies)} s\nMedian {np.median(latencies)} s\nStandard deviation {np.std(latencies)} s"
-    )
+    responses = []
+    # sequential_test()
+    parallel_test()
