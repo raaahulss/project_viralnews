68eff70eeed6862f2ad1261263f4c8a2b75de048
Gaurav <38792209+g2des@users.noreply.github.com>
2020-08-03 11:11:50 -0400

    Switching to premium

--- webapp/src/constants.py
+++ webapp/src/constants.py
@@ -2,11 +2,11 @@
 # SCRAPPING_KEY="1934f519a63e142e0d3c893e59cc37fe0172e98a"
 
 # key set 1
-CONSUMER_KEY='RHfPl9J0lOAYxFawj21tBAvgG'
-CONSUMER_SECRET='HfVVnF7fnfSCb3BHSwQ9FyAWyFa73t4wwOAIO0wZO3jhKtaYU0'
-ACCESS_TOKEN_KEY='1242854923261358083-ouJ6KVlpvIrvH0hJYjmyYceIUzuf5E'
-ACCESS_TOKEN_SECRET='AdEh0q4TBTQ4wm9mOU6Uld8HBZGfAvgC32H1YjNelq1Xk'
-SEARCH_ENV = 'projectidk' # Search environment name
+# CONSUMER_KEY='RHfPl9J0lOAYxFawj21tBAvgG'
+# CONSUMER_SECRET='HfVVnF7fnfSCb3BHSwQ9FyAWyFa73t4wwOAIO0wZO3jhKtaYU0'
+# ACCESS_TOKEN_KEY='1242854923261358083-ouJ6KVlpvIrvH0hJYjmyYceIUzuf5E'
+# ACCESS_TOKEN_SECRET='AdEh0q4TBTQ4wm9mOU6Uld8HBZGfAvgC32H1YjNelq1Xk'
+# SEARCH_ENV = 'projectidk' # Search environment name
 
 #key set 2
 # CONSUMER_KEY='fZka0Vfn7Voxt65qmSdc39QVh'
@@ -26,15 +26,15 @@ SEARCH_ENV = 'projectidk' # Search environment name
 # SEARCH_ENV = 'dev' # Search environment name
 
 # premium key set
-# CONSUMER_KEY='PtzUlT4l8yMKPtPHrmGCjF0iF'
-# CONSUMER_SECRET='sJlUtEwtlylp4witiC2exvU26IlXv6C15iS7B7zlXnWIXmrPa0'
-# ACCESS_TOKEN_KEY='1242221626256773120-I1w5qsMOQ4RkLJAtr4C0TR092TtXd2'
-# ACCESS_TOKEN_SECRET='stOnsbz98GwkceoufXAuObBaXzvritHwe3Blc057Kd8Vz'
-# SEARCH_ENV = 'dev' # Search environment name
+CONSUMER_KEY='PtzUlT4l8yMKPtPHrmGCjF0iF'
+CONSUMER_SECRET='sJlUtEwtlylp4witiC2exvU26IlXv6C15iS7B7zlXnWIXmrPa0'
+ACCESS_TOKEN_KEY='1242221626256773120-I1w5qsMOQ4RkLJAtr4C0TR092TtXd2'
+ACCESS_TOKEN_SECRET='stOnsbz98GwkceoufXAuObBaXzvritHwe3Blc057Kd8Vz'
+SEARCH_ENV = 'viralnews1' # Search environment name
 
 
 # 100 for standard twitter api and 500 for premium
-SEARCH_PER_REQUEST=100
+SEARCH_PER_REQUEST=500
 # Max number of replies returned by the system
 MAX_REPLY=SEARCH_PER_REQUEST * 2
 
